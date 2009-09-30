efed1594f89b9f574184c8cfa73fbace294c9c56
antscode <anthony@caterxpress.com>
2009-10-01 07:26:34 +1000

    Initial commit.

---
+++ data.js
@@ -0,0 +1,22 @@
+data = {
+    addresses: [
+        { name:"John A. Smith", address:"1236 Some Street", city:"Melbourne", state:"Victoria", amount:5, active:"yes", colors:["red"], last_login:"4/19/2007"},
+        { name: "John D. Smith", address: "1623 Some Street", city: "Melbourne", state: "Victoria", amount: 5, active: "yes", colors: ["red"], last_login: "4/19/2007" },
+        { name: "Bob C. Uncle", address: "9996 Random Road", city: "Melbourne", state: "Victoria", amount: 0, active: "maybe", colors: ["green"], last_login: "1/23/2004" },
+        { name: "Bob F. Uncle", address: "9899 Random Road", city: "Melbourne", state: "Victoria", amount: 0, active: "maybe", colors: ["green"], last_login: "1/23/2004" },
+        { name: "John G. Smith", address: "1723 Some Street", city: "Melbourne", state: "Victoria", amount: 5, active: "yes", colors: ["red"], last_login: "4/19/2007" },
+        { name: "Bob I. Uncle", address: "9909 Random Road", city: "Sydney", state: "New South Wales", amount: 0, active: "maybe", colors: ["green"], last_login: "1/23/2004" },
+        { name: "John J. Smith", address: "1623 Some Street", city: "Sydney", state: "New South Wales", amount: 5, active: "yes", colors: ["red"], last_login: "4/19/2007" },
+        { name: "Bob L. Uncle", address: "9989 Random Road", city: "Sydney", state: "New South Wales", amount: 0, active: "maybe", colors: ["green"], last_login: "1/23/2004" },
+        { name: "John M. Smith", address: "1293 Some Street", city: "Perth", state: "Western Australia", amount: 5, active: "yes", colors: ["red"], last_login: "4/19/2007" },
+        { name: "Bob O. Uncle", address: "9959 Random Road", city: "Perth", state: "Western Australia", amount: 0, active: "maybe", colors: ["green"], last_login: "1/23/2004" },
+        { name: "John P. Smith", address: "6123 Some Street", city: "Brisbane", state: "Queensland", amount: 5, active: "yes", colors: ["red"], last_login: "4/19/2007" },
+        { name: "Bob R. Uncle", address: "9989 Random Road", city: "Brisbane", state: "Queensland", amount: 0, active: "maybe", colors: ["green"], last_login: "1/23/2004" },
+        { name: "Joan B. Jones", address: "3271 Another Ave", city: "Brisbane", state: "Queensland", amount: 3, active: "no", colors: ["red", "blue"], last_login: "2/15/2006" },
+        { name: "Joan E. Jones", address: "3217 Another Ave", city: "Brisbane", state: "Queensland", amount: 3, active: "no", colors: ["red", "blue"], last_login: "2/15/2006" },
+        {name:"Joan H. Jones", address:"3241 Another Ave", city:"Darwin", state:"Nortern Territory", amount:3, active:"no", colors:["red","blue"], last_login:"2/15/2006"},
+        { name: "Joan K. Jones", address: "3721 Another Ave", city: "Darwin", state: "Nortern Territory", amount: 3, active: "no", colors: ["red", "blue"], last_login: "2/15/2006" },
+        {name:"Joan N. Jones", address:"3621 Another Ave", city:"Hobart", state:"Tasmania", amount:3, active:"no", colors:["red","blue"], last_login:"2/15/2006"},
+        { name: "Joan Q. Jones", address: "3281 Another Ave", city: "Hobart", state: "Tasmania", amount: 3, active: "no", colors: ["red", "blue"], last_login: "2/15/2006" }
+    ]
+};
